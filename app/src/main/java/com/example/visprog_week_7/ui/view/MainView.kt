f6296a63ae424c831c5b9d43027f20cdb8d51934
chris <christoperjoel1@gmail.com>
2025-10-23 07:39:13 +0700

    Main View Initial

---
+++ app/src/main/java/com/example/visprog_week_7/ui/view/MainView.kt
@@ -0,0 +1,15 @@
+package com.example.visprog_week_7.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainView() {
+
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun MainViewPreview() {
+    MainView()
+}
